ebea517cd4f44e87ed8b52c261ffd48806fd1f44
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-01-17 08:58:35 +0900

    Improve test speed by decreasing the call of `stack path`

--- test/Education/MakeThemToLearnHaskellSpec.hs
+++ test/Education/MakeThemToLearnHaskellSpec.hs
@@ -1,6 +1,7 @@
 module Education.MakeThemToLearnHaskellSpec (main, spec) where
 
 import           Test.Hspec
+import           Test.Hspec.Core.Spec (SpecM)
 
 import qualified Paths_makeThemToLearnHaskell
 
@@ -21,47 +22,57 @@ import qualified System.Process.Typed as Process
 main :: IO ()
 main = hspec spec
 
+
+newtype TestEnv = TestEnv { mtlhPath :: FilePath }
+
+prepareTestEnv :: SpecM a TestEnv
+prepareTestEnv = runIO $ do
+  tmpDir <- (</> "tmp/mtlh") <$> Dir.getCurrentDirectory
+  Env.setEnv "MAKE_THEM_TO_LEARN_HASKELL_HOME" tmpDir
+
+  (result, _) <- readProcess_ "stack path --local-install-root"
+  let binPath =
+        ByteString.unpack $ ByteString.takeWhile (not . isNewLine) result
+
+  return $ TestEnv $ binPath </> "bin" </> "mtlh"
+
 spec :: Spec
-spec =
+spec = do
+  e <- prepareTestEnv
   describe "mtlh verify" $
     describe "when the user has never shown any exercise" $ do
       it "given the correct answer of exercise 1, show SUCCESS" $ do
         answerFile <- Paths_makeThemToLearnHaskell.getDataFileName ("assets" </> "1.hs")
-        (code, out, err) <- runMtlh ["verify", answerFile]
-        code `shouldBe` ExitSuccess
+        (code, out, err) <- runMtlh e ["verify", answerFile]
         err `shouldSatisfy` ByteString.null
         out `shouldSatisfy` includes "SUCCESS"
+        code `shouldBe` ExitSuccess
 
       it "given an empty answer, show FAIL" $
-        runMtlh ["verify", "test/assets/common/empty.hs"]
+        runMtlh e ["verify", "test/assets/common/empty.hs"]
           >>= shouldExitWithHints ["HINT: This error indicates you haven't defined main function."]
 
       it "given an answer whose 'Hello, world!' is singlequoted, show FAIL" $
-        runMtlh ["verify", "test/assets/1/single-quote.hs"]
+        runMtlh e ["verify", "test/assets/1/single-quote.hs"]
           >>= shouldExitWithHints ["HINT: In Haskell, you must surround string literals with double-quote '\"'. Such as \"Hello, world\"."]
 
       it "given an answer whose 'Hello, world!' is singlequoted and putStrLn isn't assigned to main, show FAIL" $
-        runMtlh ["verify", "test/assets/1/single-quote-no-main.hs"]
+        runMtlh e ["verify", "test/assets/1/single-quote-no-main.hs"]
           >>= shouldExitWithHints
             [ "HINT: This error indicates you haven't defined main function."
             , "HINT: In Haskell, you must surround string literals with double-quote '\"'. Such as \"Hello, world\"."
             ]
 
       it "given an answer with typo, show FAIL" $
-        runMtlh ["verify", "test/assets/1/typo.hs"]
+        runMtlh e ["verify", "test/assets/1/typo.hs"]
           >>= shouldExitWithHints ["HINT: you might have misspelled 'putStrLn'."]
 
 
-runMtlh :: [String] -> IO (ExitCode, ByteString, ByteString)
-runMtlh args = do
-  (result, _) <- readProcess_ "stack path --local-install-root"
-  let binPath = ByteString.unpack $ ByteString.takeWhile (not . isNewLine) result
-  tmpDir <- (</> "tmp/mtlh") <$> Dir.getCurrentDirectory
-  Env.setEnv "MAKE_THEM_TO_LEARN_HASKELL_HOME" tmpDir
-  let cfg =
-        Process.proc (binPath </> "bin" </> "mtlh") args
-          & Process.setStdin Process.closed
-  readProcess cfg
+runMtlh :: TestEnv -> [String] -> IO (ExitCode, ByteString, ByteString)
+runMtlh e args =
+  readProcess $
+    Process.proc (mtlhPath e) args
+      & Process.setStdin Process.closed
 
 
 isNewLine :: Char -> Bool
