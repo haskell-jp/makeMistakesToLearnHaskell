30b56feb276ccb934eb8dc25442ded93eadbbbbd
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-04-14 22:23:44 +0900

    Make integration tests faster with main-tester

--- src/test/imports/external.hs
+++ src/test/imports/external.hs
@@ -1,8 +1,14 @@
 import           Test.Hspec
 import           Test.Hspec.Core.Spec (SpecM)
+import           Test.Main
+                   ( captureProcessResult
+                   , withArgs
+                   , ProcessResult(ProcessResult, prStdout, prStderr, prExitCode)
+                   )
 
 import qualified Paths_makeMistakesToLearnHaskell
 
+import           Control.Exception (try)
 import           Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as ByteString
 import qualified Data.ByteString.Char8 as ByteString'
@@ -12,5 +18,3 @@ import qualified System.Directory as Dir
 import qualified System.Environment as Env
 import           System.Exit (ExitCode(ExitSuccess, ExitFailure))
 import           System.FilePath ((</>))
-import           System.Process.Typed (readProcess, readProcess_)
-import qualified System.Process.Typed as Process
--- test/Education/MakeMistakesToLearnHaskellSpec.hs
+++ test/Education/MakeMistakesToLearnHaskellSpec.hs
@@ -4,6 +4,8 @@ module Education.MakeMistakesToLearnHaskellSpec (main, spec) where
 
 #include <test/imports/external.hs>
 
+import qualified Education.MakeMistakesToLearnHaskell
+
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -11,55 +13,33 @@ main :: IO ()
 main = hspec spec
 
 
-newtype TestEnv = TestEnv { mmlhPath :: FilePath }
-
-
-prepareTestEnv :: SpecM a TestEnv
-prepareTestEnv = runIO $ do
-  tmpDir <- (</> "tmp/mmlh") <$> Dir.getCurrentDirectory
-  Env.setEnv "MAKE_MISTAKES_TO_LEARN_HASKELL_HOME" tmpDir
-
-  (result, _) <- readProcess_ "stack path --local-install-root"
-  let binPath =
-        ByteString.unpack $ ByteString.takeWhile (not . isNewLine) result
-
-  return $ TestEnv $ binPath </> "bin" </> "mmlh"
-
 spec :: Spec
-spec = do
-  e <- prepareTestEnv
+spec =
   describe "mmlh verify" $
     describe "when the user has never shown any exercise" $ do
       it "given the correct answer of exercise 1, show SUCCESS" $ do
         answerFile <- Paths_makeMistakesToLearnHaskell.getDataFileName ("assets" </> "1.hs")
-        (code, out, err) <- runMmlh e ["verify", answerFile]
-        err `shouldSatisfy` ByteString.null
+        ProcessResult out err code <- runMmlh ["verify", answerFile]
+        err `shouldSatisfy` ByteString'.null
         out `shouldSatisfy` includes "SUCCESS"
         code `shouldBe` ExitSuccess
 
       it "given an empty answer, show FAIL" $
-        runMmlh e ["verify", "test/assets/common/empty.hs"]
+        runMmlh ["verify", "test/assets/common/empty.hs"]
           >>= shouldExitWithHints ["HINT: This error indicates you haven't defined main function."]
 
 
-runMmlh :: TestEnv -> [String] -> IO (ExitCode, ByteString, ByteString)
-runMmlh e args =
-  readProcess $
-    Process.proc (mmlhPath e) args
-      & Process.setStdin Process.closed
+runMmlh :: [String] -> IO ProcessResult
+runMmlh args =
+  withArgs args $ captureProcessResult Education.MakeMistakesToLearnHaskell.main
 
 
-isNewLine :: Char -> Bool
-isNewLine c = c == '\n' || c == '\r'
+includes :: ByteString'.ByteString -> ByteString'.ByteString -> Bool
+includes = ByteString'.isInfixOf
 
 
-includes :: ByteString -> ByteString -> Bool
-includes s =
-  ByteString'.isInfixOf (ByteString.toStrict s) . ByteString.toStrict
-
-
-shouldExitWithHints :: [ByteString] -> (ExitCode, ByteString, ByteString) -> IO ()
-shouldExitWithHints hintMsgs (code, out, err) = do
-  err `shouldSatisfy` ByteString.null
+shouldExitWithHints :: [ByteString'.ByteString] -> ProcessResult -> IO ()
+shouldExitWithHints hintMsgs (ProcessResult out err code) = do
+  err `shouldSatisfy` ByteString'.null
   mapM_ ((out `shouldSatisfy`) . includes) hintMsgs
   code `shouldBe` ExitFailure 1
