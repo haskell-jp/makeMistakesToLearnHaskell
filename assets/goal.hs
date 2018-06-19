de3af9a567e851bd28ac460355c7b109d9a19466
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-06-20 08:55:56 +0900

    Rewrite next goal
    The Guess Number game is not good to show functional programming style

--- assets/goal.hs
+++ assets/goal.hs
@@ -1,43 +1,61 @@
-import           System.Environment (getArgs)
-import           System.IO (stdout, hSetBuffering, BufferMode(..))
-import           Text.Read (readMaybe)
+#!/bin/env stack
+{-
+  stack script --resolver=lts-11.13
+    --package split
+    --package containers
+-}
+import Data.List.Split
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
--- TODO: 短くした例も書く
+-- import Data.Maybe
+import Data.Traversable (for)
+import System.Environment (getArgs)
+import System.IO (BufferMode(..), hSetBuffering, stdout)
 
+-- import Text.Read (readMaybe)
+type Entry = (String, Int)
+
+{-
+data Entry = Entry
+  { category :: String
+  , value :: Int
+  }
+-}
 main :: IO ()
 main = do
-  hSetBuffering stdout NoBuffering
-  args <- getArgs
-  case args of
-      (first : _) -> do
-        let maybeAnswer = readMaybe first
-        case maybeAnswer of
-            Just answer -> do
-              gameLoop answer
-            Nothing -> do
-              error ("ERROR: '" ++ show first ++ "' is not a number!")
-      [] -> do
-        error "Give answer"
+  files <- getArgs
+  entries <-
+    for files $ \path -> do
+      contents <- readFile path
+      return (parseEntries contents)
+  let summary = Map.fromListWith (+) (concat entries)
+  putStr (formatSummry summary)
 
+parseEntries :: String -> [Entry]
+parseEntries s = map parseEntry (lines s)
 
-gameLoop :: Int -> IO ()
-gameLoop answer = do
-  putStrLn "Enter an answer"
-  line <- getLine
-  let maybePlayerAnswer = readMaybe line
-  case maybePlayerAnswer of
-      Just playerAnswer -> do
-        if playerAnswer == answer then do
-          putStrLn "Right!"
-        else if playerAnswer < answer then do
-          putStrLn "Too small!"
-          putStrLn "Try once more!"
-          gameLoop answer
-        else do
-          putStrLn "Too large!"
-          putStrLn "Try once more!"
-          gameLoop answer
-      Nothing -> do
-        putStrLn ("ERROR: " ++ show line ++ " is not a number!")
-        putStrLn "Try once more!"
-        gameLoop answer
+parseEntry :: String -> Entry
+parseEntry s =
+  case splitOn "\t" s of
+    [c, sv] -> (c, read sv) -- (Entry c (read sv)
+    _ -> error $ "Invalid entry: " ++ show s
+
+{-
+parseEntries :: String -> [Entry]
+parseEntries s = mapMaybe parseEntry (lines s)
+
+parseEntry :: String -> Maybe Entry
+parseEntry s =
+  case splitOn "\t" s of
+    [c, sv] ->
+      case readMaybe sv of
+        Just v -> Just $ Entry c v
+        Nothing -> Nothing
+    _ -> Nothing
+-}
+formatSummry :: Map String Int -> String
+formatSummry summary = unlines (map formatSummaryItem (Map.toList summary))
+
+formatSummaryItem :: (String, Int) -> String
+formatSummaryItem (cat, total) = cat ++ "\t" ++ show total
