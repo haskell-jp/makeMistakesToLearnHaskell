13e1e744db8f59c1e41854bf8e7339ed50069837
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-07-16 20:23:12 +0900

    Shell script to copy from correct answer to many wrong answers

---
+++ scripts/gen-wrong-sources-from-template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+template_file="$1"
+shift
+
+error_names="$@"
+
+template_basename="$(basename "$template_file" | cut -f 1 -d '.')"
+
+for error_name in "${error_names[@]}" ; do
+  cp "$template_file" test/assets/"$template_basename"/"$error_name".hs
+done
