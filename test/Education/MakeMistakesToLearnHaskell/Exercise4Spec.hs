b150f0ab05f2242f9ac020693984c4eea1d1fc1e
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-09-15 23:25:29 +0900

    Generate left test cases for exercise 4

--- test/Education/MakeMistakesToLearnHaskell/Exercise4Spec.hs
+++ test/Education/MakeMistakesToLearnHaskell/Exercise4Spec.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
-module Education.MakeMistakesToLearnHaskell.Exercise3Spec
+module Education.MakeMistakesToLearnHaskell.Exercise4Spec
   ( spec
   , main
   ) where
@@ -29,32 +29,94 @@ spec = do
     let e = setRunHaskellSuccessWithStdout baseEnv out
     void $ shouldSuccess =<< Exercise.verify subject e "assets/4.hs"
 
-  it "given an answer with incornsistentindent, show FAIL" $ do
-    err <- ByteString.readFile "test/assets/4/error-messages/incornsistent-indent.txt"
-    let e = setRunHaskellFailureWithOutput baseEnv err
-    d <- shouldFail =<< Exercise.verify subject e "test/assets/4/incornsistent-indent.hs"
-    d `shouldSatisfy` Text.isInfixOf "HINT: instructions in a `do` must be in a consistent width. "
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-do"
+    ["HINT: You seem to forget to write `do`. `do` must be put before listing `putStr`s and `getContents`."]
 
-  it "given an answer without do, show FAIL" $ do
-    err <- ByteString.readFile "test/assets/4/error-messages/no-do.txt"
-    let e = setRunHaskellFailureWithOutput baseEnv err
-    d <- shouldFail =<< Exercise.verify subject e "test/assets/4/no-do.hs"
-    d `shouldSatisfy` Text.isInfixOf "HINT: You seem to forget to write `do`. `do` must be put before listing `putStrLn`s."
+  itShouldFailForCaseWithMessage
+    "4"
+    "equal"
+    ["HINT: Don't assign the result of `getContents` with `=`. Use `<-` instead."]
 
-  it "given an answer whose 'Hello, world!' is singlequoted, show FAIL" $ do
-    err <- ByteString.readFile "test/assets/4/error-messages/single-quote.txt"
-    let e = setRunHaskellFailureWithOutput baseEnv err
-    d <- shouldFail =<< Exercise.verify subject e "test/assets/4/single-quote.hs"
-    d `shouldSatisfy` Text.isInfixOf "HINT: In Haskell, you must surround string literals with double-quote '\"'. Such as \"Hello, world\"."
+  itShouldFailForCaseWithMessage
+    "4"
+    "incornsistent-indent1"
+    ["HINT: instructions in a `do` must be in a consistent width."]
 
-  it "given an answer with typo, show FAIL" $ do
-    err <- ByteString.readFile "test/assets/4/error-messages/typo.txt"
-    let e = setRunHaskellFailureWithOutput baseEnv err
-    void (shouldFail =<< Exercise.verify subject e "test/assets/4/typo.hs")
+  itShouldFailForCaseWithMessage
+    "4"
+    "incornsistent-indent2"
+    ["HINT: instructions in a `do` must be in a consistent width."]
 
-  it "given an answer printing wrong result, show FAIL" $ do
-    err <- ByteString.readFile "test/assets/4/error-messages/wrong-output.txt"
-    let e = setRunHaskellSuccessWithStdout baseEnv err
-    d <- shouldFail =<< Exercise.verify subject e "test/assets/4/wrong-output.hs"
-    d `shouldSatisfy` Text.isInfixOf "Your program's output:" -- TODO: better output
-    d `shouldSatisfy` Text.isInfixOf "Expected output:"
+  itShouldFailForCaseWithMessage
+    "4"
+    "main-arrow"
+    ["HINT: Don't use `<-` to define the `main` function. Use `=` instead."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-arrow"
+    [ "HINT: Unfortunately, you have to assign the result of `getContents` with `<-` operator."
+    , "HINT: You seem to forget to write `do`. `do` must be put before listing `putStr`s and `getContents`."
+    ]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-close-paren"
+    ["HINT: you might have forgot to write close parenthesis"]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-dash"
+    ["HINT: So close! Don't assign the result of `getContents` with `<`, but `<-`!"]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-main"
+    ["HINT: This error indicates you haven't defined main function."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-open-paren1"
+    ["HINT: Close! You might have forgot to write open parenthesis between `putStr` and `unlines`."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-open-paren2"
+    ["HINT: Close! You might have forgot to write open parenthesis between `unlines` and `reverse`."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-open-paren3"
+    ["HINT: Close! You might have forgot to write open parenthesis between `reverse` and `lines`."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-paren"
+    ["HINT: Unfortunately, you have to surround each function calls with parentheses! E.g. (func1 (func2 input))"]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-paren1"
+    ["HINT: You might have forgot to surround `unlines` and its argument with parentheses."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-paren2"
+    ["HINT: You might have forgot to surround `reverse` and its argument with parentheses."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "no-paren3"
+    ["HINT: You might have forgot to surround `lines` and its argument with parentheses."]
+
+  itShouldFailForCaseWithMessage
+    "4"
+    "thick-arrow"
+    ["HINT: Close! Don't assign the result of `getContents` with `<=`, but `<-`!"]
+
+  itShouldFailForCaseWithMessage -- TODO: better error message
+    "4"
+    "wrong-output"
+    ["Your program's output:", "Expected output:"]
