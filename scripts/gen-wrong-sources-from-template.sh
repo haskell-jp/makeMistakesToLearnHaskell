abc98f6531c2eada4d7976e5d5b01173a6099c5e
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-09-15 23:22:02 +0900

    fix: Properly quote array
    But I remember the it worked even before this commit...

--- scripts/gen-wrong-sources-from-template.sh
+++ scripts/gen-wrong-sources-from-template.sh
@@ -9,6 +9,6 @@ error_names="$@"
 
 template_basename="$(basename "$template_file" | cut -f 1 -d '.')"
 
-for error_name in "${error_names[@]}" ; do
+for error_name in ${error_names[@]} ; do
   cp "$template_file" test/assets/"$template_basename"/"$error_name".hs
 done
