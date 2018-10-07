6cdd706dea6f170a146b79b2dfe3db08148298f0
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-10-07 16:00:51 +0900

    Simplify the goal app's source
    Dollar won't be taught at the time of the exercise
    
    Delete comments, containing the advanced version

--- assets/goal.hs
+++ assets/goal.hs
@@ -8,28 +8,21 @@ import Data.List.Split
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
--- import Data.Maybe
 import Data.Traversable (for)
 import System.Environment (getArgs)
 import System.IO (BufferMode(..), hSetBuffering, stdout)
 
--- import Text.Read (readMaybe)
 type Entry = (String, Int)
 
-{-
-data Entry = Entry
-  { category :: String
-  , value :: Int
-  }
--}
 main :: IO ()
 main = do
   files <- getArgs
   entries <-
-    for files $ \path -> do
+    for files (\path -> do
       contents <- readFile path
       return (parseEntries contents)
-  let summary = Map.fromListWith (+) (concat entries)
+    )
+  let summary = Map.fromListWith (\x y -> x + y) (concat entries)
   putStr (formatSummry summary)
 
 parseEntries :: String -> [Entry]
@@ -38,22 +31,9 @@ parseEntries s = map parseEntry (lines s)
 parseEntry :: String -> Entry
 parseEntry s =
   case splitOn "\t" s of
-    [c, sv] -> (c, read sv) -- (Entry c (read sv)
-    _ -> error $ "Invalid entry: " ++ show s
+    [c, sv] -> (c, read sv)
+    _ -> error ("Invalid entry: " ++ show s)
 
-{-
-parseEntries :: String -> [Entry]
-parseEntries s = mapMaybe parseEntry (lines s)
-
-parseEntry :: String -> Maybe Entry
-parseEntry s =
-  case splitOn "\t" s of
-    [c, sv] ->
-      case readMaybe sv of
-        Just v -> Just $ Entry c v
-        Nothing -> Nothing
-    _ -> Nothing
--}
 formatSummry :: Map String Int -> String
 formatSummry summary = unlines (map formatSummaryItem (Map.toList summary))
 
