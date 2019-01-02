66c57e98327b8334f548e707a4bbef439ec0b7f5
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-01-02 15:29:36 +0900

    課題5にletの説明を追加
    Fix https://github.com/haskell-jp/makeMistakesToLearnHaskell/issues/93

--- assets/5.hs
+++ assets/5.hs
@@ -1,5 +1,11 @@
 main = do
-  principal <- getLine
-  interestRate <- getLine
-  years <- getLine
-  print (read principal * (1 + read interestRate / 100) ^ read years)
+  line1 <- getLine
+  let principal = read line1
+
+  line2 <- getLine
+  let interestRate = read line2
+
+  line3 <- getLine
+  let years = read line3
+
+  print (principal * (1 + interestRate / 100) ^ years)
