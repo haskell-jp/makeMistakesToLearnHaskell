8eab37e3caa2ccc48918b563e1bbe2d4fb411f47
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-05-07 09:26:11 +0900

    (WIP) Add tests for the report feature

--- test/Education/MakeMistakesToLearnHaskellSpec.hs
+++ test/Education/MakeMistakesToLearnHaskellSpec.hs
@@ -18,43 +18,70 @@ spec :: Spec
 spec =
   describe "mmlh verify" $ do
     it "given the correct answer of exercise 1, show SUCCESS" $ do
-      void $ runMmlh ["show", "--terminal", "1"]
+      void $ runMmlh ["show", "--terminal", "1"] ""
       answerFile <- Paths.getDataFileName ("assets" </> "1.hs")
-      runMmlh ["verify", answerFile] >>= shouldVerifySuccess
+      runMmlh ["verify", answerFile] "" >>= shouldVerifySuccess
 
     it "given an empty answer, show FAIL" $ do
-      void $ runMmlh ["show", "--terminal", "1"]
-      runMmlh ["verify", "test/assets/common/empty.hs"]
-        >>= shouldExitWithHints ["HINT: This error indicates you haven't defined main function."]
+      void $ runMmlh ["show", "--terminal", "1"] ""
+      runMmlh ["verify", "test/assets/common/empty.hs"] ""
+        >>= shouldExitWithMessages ["HINT: This error indicates you haven't defined main function."]
 
     it "given non-existing answer of exercise 2.5, show NOT VERIFIED" $ do
-      void $ runMmlh ["show", "--terminal", "2.5"]
-      runMmlh ["verify", "non-existing"] >>= shouldPrintNotVerified
+      void $ runMmlh ["show", "--terminal", "2.5"] ""
+      runMmlh ["verify", "non-existing"] "" >>= shouldPrintNotVerified
 
     it "given the correct answer of exercise 4, show SUCCESS" $ do
       answerFile <- Paths.getDataFileName ("assets" </> "4.hs")
-      void $ runMmlh ["show", "--terminal", "4"]
-      runMmlh ["verify", answerFile] >>= shouldVerifySuccess
+      void $ runMmlh ["show", "--terminal", "4"] ""
+      runMmlh ["verify", answerFile] "" >>= shouldVerifySuccess
 
     it "given a wrong answer of exercise 4, show FAIL" $ do
       let msgs = ["Your program's output:", "Expected output:"]
-      void $ runMmlh ["show", "--terminal", "4"]
-      runMmlh ["verify", "test/assets/4/wrong-output.hs"]
-        >>= shouldExitWithHints msgs
+      void $ runMmlh ["show", "--terminal", "4"] ""
+      runMmlh ["verify", "test/assets/4/wrong-output.hs"] ""
+        >>= shouldExitWithMessages msgs
 
     it "given a not-compilable answer of exercise 4, show FAIL" $ do
       let msgs =
             ["HINT: You seem to forget to write `do`. `do` must be put before listing `putStr`s and `getContents`."]
-      void $ runMmlh ["show", "--terminal", "4"]
-      runMmlh ["verify", "test/assets/4/no-do.hs"]
-        >>= shouldExitWithHints msgs
+      void $ runMmlh ["show", "--terminal", "4"] ""
+      runMmlh ["verify", "test/assets/4/no-do.hs"] ""
+        >>= shouldExitWithMessages msgs
 
 
-runMmlh :: [String] -> IO ProcessResult
-runMmlh args = do
+    let expectedGitHubUrl n =
+          "https://github.com/haskell-jp/makeMistakesToLearnHaskell-support/issues/new?labels=support&title=Exercise%20" <> n <> "&body="
+
+    context "given \"y\" from stdin" $ do
+      it "given a not-compilable answer of exercise 5, show FAIL with the URL to submit an issue to haskell-jp/" $ do
+        void $ runMmlh ["show", "--terminal", "5"] ""
+        runMmlh ["verify", "test/assets/common/empty.hs"] "y\n"
+          >>= shouldExitWithMessages [expectedGitHubUrl "5"]
+
+      it "given a not-compilable answer of exercise 6, show FAIL" $ do
+        void $ runMmlh ["show", "--terminal", "6"] ""
+        runMmlh ["verify", "test/assets/common/empty.hs"] "y\n"
+          >>= shouldExitWithMessages [expectedGitHubUrl "6"]
+
+    context "given nothing from stdin" $ do
+      it "given a not-compilable answer of exercise 5, show FAIL without any URL to GitHub." $ do
+        void $ runMmlh ["show", "--terminal", "5"] ""
+        runMmlh ["verify", "test/assets/common/empty.hs"] ""
+          >>= shouldNotExitWithMessages [expectedGitHubUrl "5"]
+
+      it "given a not-compilable answer of exercise 6, show FAIL without any URL to GitHub." $ do
+        void $ runMmlh ["show", "--terminal", "6"] ""
+        runMmlh ["verify", "test/assets/common/empty.hs"] ""
+          >>= shouldNotExitWithMessages [expectedGitHubUrl "6"]
+
+
+runMmlh :: [String] -> ByteString'.ByteString -> IO ProcessResult
+runMmlh args stdinDat = do
   Dir.createDirectoryIfMissing False "./tmp/"
-  let env = [ (homePathEnvVarName, Just "./tmp/")]
+  let env = [(homePathEnvVarName, Just "./tmp/")]
   withArgs args
+    $ withStdin stdinDat
     $ withEnv env
     $ captureProcessResult Education.MakeMistakesToLearnHaskell.main
 
@@ -63,12 +90,24 @@ includes :: ByteString'.ByteString -> ByteString'.ByteString -> Bool
 includes = ByteString'.isInfixOf
 
 
-shouldExitWithHints :: [ByteString'.ByteString] -> ProcessResult -> IO ()
-shouldExitWithHints hintMsgs (ProcessResult out err code ex) = do
+shouldExitWithMessagesLike
+  :: (ByteString'.ByteString -> ByteString'.ByteString -> Bool)
+  -> [ByteString'.ByteString]
+  -> ProcessResult
+  -> IO ()
+shouldExitWithMessagesLike p hintMsgs (ProcessResult out err code ex) = do
   fmap show ex `shouldBe` Nothing
   err `shouldSatisfy` ByteString'.null
-  mapM_ ((out `shouldSatisfy`) . includes) hintMsgs
+  mapM_ ((out `shouldSatisfy`) . p) hintMsgs
   code `shouldBe` ExitFailure 1
+
+
+shouldExitWithMessages :: [ByteString'.ByteString] -> ProcessResult -> IO ()
+shouldExitWithMessages = shouldExitWithMessagesLike includes
+
+
+shouldNotExitWithMessages :: [ByteString'.ByteString] -> ProcessResult -> IO ()
+shouldNotExitWithMessages = shouldExitWithMessagesLike (\s -> not . includes s)
 
 
 shouldVerifySuccess :: ProcessResult -> IO ()
