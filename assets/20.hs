aaed4683cb6ee856b18cbf57fae582a221070167
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-08-28 09:13:37 +0900

    Update assets/20.hs

--- assets/20.hs
+++ assets/20.hs
@@ -3,7 +3,7 @@ main =
   print =<< (yearlyRate <$> (read <$> getLine) <*> (read <$> getLine) <*> (read <$> getLine))
 
 
--- https://support.microsoft.com/ja-jp/help/141695/xl-how-to-calculate-compound-interest
+-- Ref: https://support.microsoft.com/ja-jp/help/141695/xl-how-to-calculate-compound-interest
 yearlyRate :: Double -> Double -> Integer -> Double
 yearlyRate principal interestRate years =
   principal * (1 + interestRate / 100) ^ years
