b411d023f1aeda22c0097f7d4cfbb1c54b133c3b
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-07-15 18:21:19 +0900

    (WIP) (WON'T COMPILE) Begin to write judge and genrator of ex15

--- src/Education/MakeMistakesToLearnHaskell/Exercise/Ex15.hs
+++ src/Education/MakeMistakesToLearnHaskell/Exercise/Ex15.hs
@@ -11,26 +11,37 @@ import Education.MakeMistakesToLearnHaskell.Exercise.Types
 
 
 exercise15 :: Exercise
-exercise15 = Exercise "15" notYetImplementedVeirificationExercise
+exercise15 = Exercise "15"
+           $ runHaskellExerciseWithArgsEq diag answer argsGenerator
 
 
-{-
 diag :: Diagnosis
 diag _code _msg = "" -- TODO: Not implemented
 
 
-generator :: Gen String
-generator =
-  unlines <$> sequence
-    [ show <$> (arbitrary :: Gen Double)
-    , show <$> (arbitrary :: Gen Double)
-    , show . QuickCheck.getPositive <$> (arbitrary :: Gen (QuickCheck.Positive Int))
-    ]
+argsGenerator :: Gen [String]
+argsGenerator = QuickCheck.listOf nonEmptyString
+ where
+  -- NOTE: On Windows, passing an empty string in command line arguments is hard.
+  nonEmptyString =
+    QuickCheck.listOf1
+      . QuickCheck.elements
+      $ ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9']
 
 
-answer :: Text -> Text
-answer input = Text.pack $ show (body :: Double) <> "\n"
-  where
-    [principal, interestRate, years] = lines $ Text.unpack input
-    body = read principal * (1 + read interestRate / 100) ^ (read years :: Integer)
--}
+judge :: Judge
+judge args _input _exitCode actualOut =
+  case (exitCode, answer args) of
+      (ExitSuccess, Right expectedOut) -> (expectedOut, actualOut == expectedOut)
+      (ExitFailure _, Left expectedOut) -> (expectedOut, expectedOut `Text.isInfixOf` actualOut)
+      (_, Right expectedOut) -> (expectedOut, False)
+      (_, Left expectedOut) -> (expectedOut, False)
+
+
+answer :: [String] -> IO Text
+answer args =
+  for_ args (\arg -> do
+    putStrLn arg
+    content <- readFile arg -- TODO: Create temporary file for input with generated names
+    let indentedLines = map (\l -> "  " <> l) (lines content)
+    putStr (unlines indentedLines))
