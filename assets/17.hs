7cd2608eeadcbff495a09f58eb0e17f8e33653b1
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-06-18 18:24:19 +0900

    Begin to rewrite the last section of ex17

--- assets/17.hs
+++ assets/17.hs
@@ -6,35 +6,24 @@ main = go M.empty
   go m = do
     putStrLn "Enter command:"
     command <- getLine
-    case parseCommand command of
-        Just (Add k v) -> do
-          putStrLn ("Adding " ++ show v ++ " to " ++ show k ++ ".")
-          go (M.insertWith (\v1 v2 -> v1 + v2) k v m)
-        Just (Get k) -> do
+    case words command of
+        ["add", k, mv] -> do
+          case readMaybe mv of
+            Just v -> do
+              putStrLn ("Adding " ++ show v ++ " to " ++ show k ++ ".")
+              go (M.insertWith (\v1 v2 -> v1 + v2) k v m)
+            Nothing -> do
+              putStrLn "Error: Invalid Command"
+              go m
+        ["get", k] -> do
           case M.lookup k m of
               Just v ->
                 putStrLn (k ++ " => " ++ show v)
               _ ->
                 putStrLn "Error: no item found"
           go m
-        Just Quit ->
+        ["quit"] ->
           putStrLn "Bye."
-        Nothing -> do
+        _ -> do
           putStrLn "Error: Invalid Command"
           go m
-
-data Command =
-    Add String Integer
-  | Get String
-  | Quit
-
-parseCommand :: String -> Maybe Command
-parseCommand s =
-  case words s of
-      ["add", k, mv] ->
-        case readMaybe mv of
-            Just v -> Just (Add k v)
-            Nothing -> Nothing
-      ["get", k] -> Just (Get k)
-      ["quit"] -> Just Quit
-      _ -> Nothing
