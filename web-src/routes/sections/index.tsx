64bf96d034c48070ff9c25504c888a48116522ca
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2026-03-22 21:44:09 +0900

    list and show sections

--- web-src/routes/sections/index.tsx
+++ web-src/routes/sections/index.tsx
@@ -3,26 +3,35 @@ import { routeLoader$ } from "@builder.io/qwik-city";
 
 type SectionId = string;
 
-type SectionLink = { id: SectionId; path: string };
+type SectionLink = {
+  id: SectionId;
+  path: string;
+};
+
+type SectionLinks = SectionLink[];
 
 // Probably false positive:
 // eslint-disable-next-line qwik/loader-location
-const useSectionLinks = routeLoader$((): SectionLink[] => {
-  const mods = import.meta.glob("../../../assets/*.md", { eager: true });
-  const sectionIds: SectionLink[] = [];
-
-  for (const path in mods) {
+const useSectionLinks = routeLoader$((): SectionLinks => {
+  const mods = import.meta.glob("./*/index.md", { eager: true });
+  const sectionLinks: SectionLinks = [];
+  for (const [modPath, mod_] of Object.entries(mods)) {
     // FIXME: add a concrete type
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const mod = mods[path] as any;
-    const id = path.match(/([^/]*)\.md$/)![1];
-    sectionIds.push({ id, path: `/sections/${id}` });
+    const mod = mod_ as any;
+    console.log(mod.headings);
+    const id = modPath.match(/([^/]*)\/index\.md$/)![1];
+    sectionLinks.push({
+      id,
+      path: modPath.replace(/\/index.md$/, ""),
+    });
   }
-  return sectionIds;
+  return sectionLinks;
 });
 
 export default component$(() => {
   const sectionLinks = useSectionLinks();
+  console.log(sectionLinks);
 
   return (
     <div>
