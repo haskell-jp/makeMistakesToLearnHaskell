50fe5a5e86e0b5e4b971bf5f39a26bf51447de74
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2026-03-22 20:54:06 +0900

    (NOT WORKING) try to keep the existing directory structure

--- web-src/routes/sections/index.tsx
+++ web-src/routes/sections/index.tsx
@@ -1,35 +1,34 @@
-import { component$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 type SectionId = string;
 
-type SectionLinks = Map<SectionId, string>;
+type SectionLink = { id: SectionId; path: string };
 
 // Probably false positive:
 // eslint-disable-next-line qwik/loader-location
-const useSectionIds = routeLoader$((): SectionLinks => {
-  const mods = import.meta.glob('./*.md', { eager: true });
-  const sectionIds: SectionLinks = new Map();
-  
+const useSectionLinks = routeLoader$((): SectionLink[] => {
+  const mods = import.meta.glob("../../../assets/*.md", { eager: true });
+  const sectionIds: SectionLink[] = [];
+
   for (const path in mods) {
     // FIXME: add a concrete type
-    // eslint-disable-next-line @typescript/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const mod = mods[path] as any;
-    console.log(mod.headings);
-    const id = path.replace('./', '').replace('.md', '');
-    sectionIds.set(id, path);
+    const id = path.match(/([^/]*)\.md$/)![1];
+    sectionIds.push({ id, path: `/sections/${id}` });
   }
   return sectionIds;
 });
 
 export default component$(() => {
-  const sectionIds = useSectionIds();
-  
+  const sectionLinks = useSectionLinks();
+
   return (
     <div>
       <h1>Sections</h1>
       <ul>
-        {[...sectionIds.value.entries()].map(([id, path]) => (
+        {sectionLinks.value.map(({ id, path }) => (
           <li key={id}>
             <a href={path}>{id}</a>
           </li>
