75ce616c8ebd521595c42c11e7bd0317b5c3cf4a
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2026-08-02 17:27:20 +0900

    correct root directory of rootfs.tar

--- scripts/build-mmlhc.sh
+++ scripts/build-mmlhc.sh
@@ -5,33 +5,32 @@ set -euxo pipefail
 # Original: https://gitlab.haskell.org/ghc/ghc/-/blob/af40606aaaf65c112e67cb4e4c37aec4ea74fb36/testsuite/tests/ghc-api-browser/playground001.sh
 # Copyright 2002, The University Court of the University of Glasgow. All rights reserved.
 
-cd "$(dirname "$0")/.."
+project_root="$(realpath "$(dirname "$0")"/..)"
 
-outdir=./web-src/assets/ghc
-tmpdir=./web-src/assets/ghc/tmp
+cd "$project_root/web-src/assets/ghc"
 
-mkdir -p "$tmpdir"
+mkdir -p tmp
 
-wasm32-wasi-ghc -O2 -package ghc -shared -dynamic -no-keep-hi-files -no-keep-o-file -o "$tmpdir/libmmlhc.so" c-src/mmlhc.hs
+wasm32-wasi-ghc -O2 -package ghc -shared -dynamic -no-keep-hi-files -no-keep-o-file -o tmp/libmmlhc.so "$project_root/c-src/mmlhc.hs"
 
-# $tmpdir/clib contains libc/libc++ .so files
-WASI_SDK="$(nix eval --raw 'gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org#wasi-sdk')"
-cp -r "$WASI_SDK"/share/wasi-sysroot/lib/wasm32-wasi "$tmpdir"/clib
-chmod -R 0777 "$tmpdir"/clib
+# tmp/clib contains libc/libc++ .so files
+WASI_SDK="$(nix build --no-link --print-out-paths 'gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org#wasi-sdk')"
+cp -r "$WASI_SDK"/share/wasi-sysroot/lib/wasm32-wasi tmp/clib
+chmod -R 0777 tmp/clib
 # trim unneeded stuff in c libdir
-find "$tmpdir"/clib -type f ! -name "*.so" -delete
-rm -f "$tmpdir"/clib/libsetjmp.so "$tmpdir"/clib/libwasi-emulated-*.so
+find tmp/clib -type f ! -name "*.so" -delete
+rm -f tmp/clib/libsetjmp.so tmp/clib/libwasi-emulated-*.so
 
-mkdir -p "$tmpdir"/hslib
-cp -r "$(wasm32-wasi-ghc --print-libdir)" "$tmpdir"/hslib/lib
-chmod -R 0777 "$tmpdir"/hslib
+mkdir -p tmp/hslib
+cp -r "$(wasm32-wasi-ghc --print-libdir)" tmp/hslib/lib
+chmod -R 0777 tmp/hslib
 # unregister Cabal/Cabal-syntax, too big
-rm "$tmpdir"/hslib/lib/package.conf.d/package.cache.lock || true
-wasm32-wasi-ghc-pkg --no-user-package-db --global-package-db="$tmpdir"/hslib/lib/package.conf.d unregister Cabal Cabal-syntax || true
-wasm32-wasi-ghc-pkg --no-user-package-db --global-package-db="$tmpdir"/hslib/lib/package.conf.d recache || true
+rm tmp/hslib/lib/package.conf.d/package.cache.lock || true
+wasm32-wasi-ghc-pkg --no-user-package-db --global-package-db=tmp/hslib/lib/package.conf.d unregister Cabal Cabal-syntax || true
+wasm32-wasi-ghc-pkg --no-user-package-db --global-package-db=tmp/hslib/lib/package.conf.d recache || true
 
 # we only need non-profiling .dyn_hi/.so, trim as much as we can
-find "$tmpdir"/hslib/lib "(" \
+find tmp/hslib/lib "(" \
   -name "*.hi" \
   -o -name "*.a" \
   -o -name "*.p_hi" \
@@ -41,19 +40,19 @@ find "$tmpdir"/hslib/lib "(" \
   -o -name "libHSrts*_debug*.so" \
   ")" -delete
 rm -rf \
-  "$tmpdir"/hslib/lib/doc \
-  "$tmpdir"/hslib/lib/html \
-  "$tmpdir"/hslib/lib/latex \
-  "$tmpdir"/hslib/lib/*.mjs \
-  "$tmpdir"/hslib/lib/*.js \
-  "$tmpdir"/hslib/lib/*.txt
+  tmp/hslib/lib/doc \
+  tmp/hslib/lib/html \
+  tmp/hslib/lib/latex \
+  tmp/hslib/lib/*.mjs \
+  tmp/hslib/lib/*.js \
+  tmp/hslib/lib/*.txt
 # HS_SEARCHDIR is something like
 # /tmp/hslib/lib/wasm32-wasi-ghc-9.15.20251024 which is the
 # dynamic-library-dirs that contains all libHS*.so in one place, and
 # also static libraries in per-unit directories
-HS_SEARCHDIR=$(find "$tmpdir"/hslib/lib -type f -name "*.so" -print0 | xargs -0 -n1 dirname | sort -u | sed "s|^\./|/|")
+HS_SEARCHDIR=$(find tmp/hslib/lib -type f -name "*.so" -print0 | xargs -0 -n1 dirname | sort -u | sed "s|^\./|/|")
 # hunt down the remaining bits of Cabal/Cabal-syntax. too bad there's
 # no ghc-pkg uninstall.
 rm -rf ."$HS_SEARCHDIR"/*Cabal*
 
-tar -cf "$outdir"/rootfs.tar.zst --zstd "$tmpdir"
+tar -cf rootfs.tar.zst --zstd tmp
